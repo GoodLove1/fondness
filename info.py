ce1e488cbd109292df8185febfac354cf898f04b
GoodLove1 <110181533+GoodLove1@users.noreply.github.com>
2023-01-12 20:48:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,16 +39,16 @@ class evamaria(Client):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID',"12635510"))
 API_HASH = environ.get('API_HASH',"da59e4e56ec4fe35af603bd30208ecc5")
-BOT_TOKEN = environ.get('BOT_TOKEN',"")
+BOT_TOKEN = environ.get('BOT_TOKEN',"5840228189:AAFdcjx5rWTJk8SGUahRtXEVq-4tT_xFGT8")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,"")).split()
+PICS = (environ.get('PICS' ,"https://telegra.ph/file/e62220405a15a20e802c8.mp4")).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS',"1130215726").split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', "1130215726").split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', "-1001887386865").split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', "").split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL',"-1001861393412")
